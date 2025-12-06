fdfd6d1d1018c6f1379d849ca0c6e1be9d95d575
LestDomzEDU <ledominguez@csumb.edu>
2025-12-05 19:07:48 -0800

    server fixes

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 // server.js - simple Express server for Heroku
 const express = require("express");
 const path = require("path");
@@ -6,14 +5,14 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// This is where Expo's web build will live
+// This is where Expo's web build will be exported
 const buildPath = path.join(__dirname, "web-build");
 
 // Serve static files (JS, CSS, images)
 app.use(express.static(buildPath));
 
-// For any route (/, /oauth, /settings, etc), send index.html
-// so React Router / Expo Router can handle it on the client.
+// Catch-all route: send index.html for any path
+// This pattern works fine with Express 4
 app.get("*", (req, res) => {
   res.sendFile(path.join(buildPath, "index.html"));
 });
